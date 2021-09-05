efee33c7df247d21dc217e2042be9b06fde8783e
Eugene Serdyuk <eugene.serdyuk@pm.me>
2021-09-05 21:55:41 +0100

    fix heroku port binding

--- portfolio_optimization/guniconf.py
+++ portfolio_optimization/guniconf.py
@@ -1,6 +1,6 @@
 import multiprocessing as mp
 import os
 
-bind = "0.0.0.0:5000"
+bind = f"0.0.0.0:{os.getenv('PORT', 5000)}"
 workers = os.getenv("WEB_CONCURRENCY", mp.cpu_count() * 2 + 1)
 worker_class = "uvicorn.workers.UvicornWorker"
